b6d19872c6c15c537ea3d86a6cd0ab9b6eca6e50
Andrey Bogachevskiy <a.bogachevskiy@efko.ru>
2020-08-12 09:26:31 +0300

    step commit

--- i_app_webpack/src/app.ts
+++ i_app_webpack/src/app.ts
@@ -1,6 +1,10 @@
 import ProjectInput from './App/Models/ProjectInput';
 import ProjectList from './App/Models/ProjectList';
 import ProjectState from './App/Models/ProjectState';
+// import _ from 'lodash'; не, работае, т.к. TS не понимает что это
+// нужно ставить, к примеру, 
+import _ from 'lodash'; // теперь работает
+
 
 const projectStateContainer = ProjectState.getInstance();
 
@@ -8,3 +12,5 @@ ProjectInput.initForm(projectStateContainer);
 
 new ProjectList('active', projectStateContainer);
 new ProjectList('finished', projectStateContainer);
+
+console.log(_.shuffle([1, 2, 3]));
